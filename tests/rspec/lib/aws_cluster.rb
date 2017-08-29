91a8af43adaaf45706a3f7884e8c6dbc7bd3310e
Max Leonard Inden <IndenML@gmail.com>
2017-08-29 10:43:47 +0200

    tests: Assume aws role in rspec tests if on Jenkins
    When we run aws tests, we should not be using the default credentials,
    but instead assume the tectonic-installer role and use those
    credentials.

--- tests/rspec/lib/aws_cluster.rb
+++ tests/rspec/lib/aws_cluster.rb
@@ -1,7 +1,14 @@
 require 'cluster'
+require 'json'
+require 'jenkins'
 
 # AWSCluster represents a k8s cluster on AWS cloud provider
 class AWSCluster < Cluster
+  def initialize(prefix, tf_vars_path)
+    assume_role if Jenkins.environment?
+    super(prefix, tf_vars_path)
+  end
+
   def env_variables
     variables = super
     variables['PLATFORM'] = 'aws'
@@ -22,5 +29,26 @@ class AWSCluster < Cluster
 
   def ssh_key_defined?
     EnvVar.set?(%w[TF_VAR_tectonic_aws_ssh_key])
+  end
+
+  def assume_role
+    role_name = ENV['TECTONIC_INSTALLER_ROLE']
+
+    role_arn = JSON.parse(
+      `aws iam get-role --role-name="#{role_name}"`
+    )['Role']['Arn']
+
+    credentials = request_credentials(role_arn)
+
+    ENV['AWS_ACCESS_KEY_ID'] = credentials['AccessKeyId']
+    ENV['AWS_SECRET_ACCESS_KEY'] = credentials['SecretAccessKey']
+    ENV['AWS_SESSION_TOKEN'] = credentials['SessionToken']
+  end
+
+  def request_credentials(role_arn)
+    cmd = "aws sts assume-role --role-arn='#{role_arn}'"\
+          ' --role-session-name=tectonic-installer'
+    puts cmd
+    JSON.parse(`#{cmd}`)['Credentials']
   end
 end
